a5d490b04b82b243af07d9055da151891bd12a35
vladimirerokhin <vovaerohin123@gmail.com>
2022-07-25 20:52:55 +0500

    Create class DrawingIntersections

--- intersectionDrawHandler.js
+++ intersectionDrawHandler.js
@@ -3,6 +3,7 @@ import {getDeltaLatLon} from "./calculateDistance.js"
 //Создать классы: DrawingLanes, DrawingConnectingLanes
 
 class DrawingConnectingLines {
+
     constructor(drawnLanes, map) {
         this.drawnLanes = drawnLanes
         this.map = map
@@ -14,24 +15,102 @@ class DrawingConnectingLines {
     //x2, y2 - средняя точка
     //x3, y3 - конечная точка
     getCoordsForConnectingLine(x1, y1, x2, y2, x3, y3) {
-
+        const arrayOfCoordsForConnectingLine = []
+        const step = 0.001
+        for (let t = 0; t < 1; t += step) {
+            const lat = Math.pow((1 - t), 2) * x1 + 2 * (1 - t) * t * x2 + Math.pow(t, 2) * x3
+            const lon = Math.pow((1 - t), 2) * y1 + 2 * (1 - t) * t * y2 + Math.pow(t, 2) * y3
+            arrayOfCoordsForConnectingLine.push([lat, lon])
+        }
+        return arrayOfCoordsForConnectingLine
     }
 
+    //Метод для соединения двух полос (с коротой съехать и на какую съехать)
+    connectLanes(firstLane, secondLane) {
 
-    drawConnectingLines() {
+        //Координаты конца у первой полосы
+        let firstLaneCoords = {
+            lat: firstLane.middleLineWithDecorator.middleLine.getLatLngs().slice(-1)[0]['lat'],
+            lon: firstLane.middleLineWithDecorator.middleLine.getLatLngs().slice(-1)[0]['lng']
+        }
 
+        //Кординаты начала у второй полосы
+        let secondLaneCoords = {
+            lat: secondLane.middleLineWithDecorator.middleLine.getLatLngs().slice(0)[0]['lat'],
+            lon: secondLane.middleLineWithDecorator.middleLine.getLatLngs().slice(0)[0]['lng']
+        }
+
+        const coordsForConnectingLine = this.getCoordsForConnectingLine(firstLaneCoords.lat, firstLaneCoords.lon,
+            57.965890, 56.244655, secondLaneCoords.lat, secondLaneCoords.lon)
+
+        const connectingLine = L.polyline(coordsForConnectingLine, {
+            weight: 1,
+            color: 'black'
+        }).addTo(this.map)
+
+        const connectingLineDecorator = L.polylineDecorator(connectingLine, {
+            patterns: [
+                {
+                    offset: '2%',
+                    repeat: 40,
+                    symbol: L.Symbol.arrowHead({
+                        pixelSize: 5,
+                        polygon: false,
+                        pathOptions: {stroke: true}
+                    })
+                }
+            ]
+        }).addTo(this.map);
+
+        firstLane.connectedLanes.push({connectingLine, connectingLineDecorator})
     }
 
-    addConnectingLinesForLane() {
+    //Метод для отрисовки линий съезда
+    drawConnectingLines(drawnLane) {
+        //Проверка на удаление
+        function checkOnRemove() {
+            return drawnLane.connectingLanes.length === drawnLane.connectedLanes.length && drawnLane.connectedLanes.length !== 0
+                || document.getElementById("p1").innerHTML > 0;
+        }
+        if (checkOnRemove()) {
+            this.removeConnectingLines(drawnLane)
+        }
+        else {
+            //Ищем полосу с нужным laneID и соединяем
+            drawnLane.forEach(anotherLane => {
+                if (drawnLane.connectingLanes.includes(anotherLane.laneID)) {
+                    this.connectLanes(drawnLane, anotherLane)
+                }
 
+                //Добавляем дополнительную информацию для каждой полосы
+                document.getElementById("p1").innerHTML =
+                    `Тип полосы: ${drawnLane.laneInfo.laneType}<br>
+                    Полосы для съезда: ${drawnLane.laneInfo.connectingLanes}<br>
+                    Направление движения: ${drawnLane.laneInfo.directionalUse}<br>
+                    ID полосы: ${drawnLane.laneInfo.laneID}<br>`
+            })
+        }
     }
 
-    removeConnectingLines() {
-
+    //По клику на любую линию будет открываться вся дополнительная информация, а также будут показываться
+    //полосы, на которые можно съехать
+    addClickEventOnLanes() {
+        this.drawnLanes.forEach(drawnLane => {
+            drawnLane.bounds.on('click', (e) => this.drawConnectingLines(drawnLane, this.map))
+            drawnLane.middleLineWithDecorator.middleLine.on('click', (e) => this.drawConnectingLines(drawnLane, this.map))
+            drawnLane.middleLineWithDecorator.middleLineDecorator.on('click', (e) => this.drawConnectingLines(drawnLane, this.map))
+        })
     }
 
-
-
+    //Метод для удаления линий съезда на другую полосу
+    removeConnectingLines(drawnLane) {
+        drawnLane.connectedLanes.forEach(connectedLane => {
+            Object.values(connectedLane).forEach(value => {
+                value.remove()
+            })
+        })
+        drawnLane.connectedLanes.length = 0
+    }
 }
 
 class DrawingLanes {
@@ -65,7 +144,7 @@ class DrawingLanes {
             bounds,
             stopLine,
             delta,
-
+            connectedLanes: []
         })
     }
 
@@ -96,6 +175,9 @@ class DrawingLanes {
         }
         return coordsOfMiddleLine
     }
+
+    //Метод для получения координат середины перекрестка
+    getCoordsForMiddlePointOnIntersection(laneData) {}
 
     //Метод для получения смещения относительно координат средней полосы
     //Используется для отрисовки ширины полосы
